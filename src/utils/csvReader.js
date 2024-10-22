c9eb851bd529529d2b51d8cd41eb6e36e243ae3b
Aarya Rajoju <arajoju@ARMBP.local>
2024-10-22 17:18:33 -0400

    updated code to work with multiple dates

--- src/utils/csvReader.js
+++ src/utils/csvReader.js
@@ -1,14 +1,22 @@
 const fs = require('fs');
 const csv = require('csv-parser');
 
-function readAnnouncements(file) {
-    const announcements = [];
+function readAnnouncements(filePath) {
     return new Promise((resolve, reject) => {
-        fs.createReadStream(file)
-          .pipe(csv())
-          .on('data', (row) => announcements.push(row))
-          .on('end', () => resolve(announcements))
-          .on('error', reject);
+        const announcements = [];
+        fs.createReadStream(filePath)
+            .pipe(csv())
+            .on('data', (row) => {
+                announcements.push({
+                    date: row.date,
+                    time: row.time,
+                    message: row.message
+                });
+            })
+            .on('end', () => {
+                resolve(announcements);
+            })
+            .on('error', reject);
     });
 }
 
