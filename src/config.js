924ac13174bd0026333e66a2a9c9fa1d5456631a
AARYA RAJOJU <30365637+aaryarajoju@users.noreply.github.com>
2025-01-20 18:53:28 -0500

    Updated config.js

--- src/config.js
+++ src/config.js
@@ -4,7 +4,6 @@ require('dotenv').config();
 
 const config = {
     discordBotToken: process.env.DISCORD_BOT_TOKEN, 
-    announcementsFile: './src/events/test.csv', 
     announcementsChannelID: '1292982632845676575',
     googleAPIKeyFile: './src/discordannouncementsbot-37dea61d1483.json',
     sheetsID: '18k0aFirAUMHFZ10agKB55lnfIkiXps4TW21pKY4q9oU', // this is only for test-run; change this with the actual link for sheets
