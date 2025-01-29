b7d1d106dc14f43260f77c06984658fb3a42e0c1
Koray Öztürkler <kerimkoray.ozturkler1@gmail.com>
2025-01-29 11:28:00 -0500

    refactored all configs into env file to avoid merge conflicts

--- src/config.js
+++ src/config.js
@@ -4,9 +4,9 @@ require('dotenv').config();
 
 const config = {
     discordBotToken: process.env.DISCORD_BOT_TOKEN, 
-    announcementsChannelID: '1292982632845676575',
-    googleAPIKeyFile: './src/discordannouncementsbot-37dea61d1483.json',
-    sheetsID: '18k0aFirAUMHFZ10agKB55lnfIkiXps4TW21pKY4q9oU', // this is only for test-run; change this with the actual link for sheets
+    announcementsChannelID: process.env.CHANNEL_ID,
+    googleAPIKeyFile: process.env.SERVICE_ACC_AUTH_PATH,
+    sheetsID: process.env.SHEETS_ID,
 };
 
 module.exports = config;
